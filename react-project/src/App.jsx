d90c073a59643b71cd39c303b4228e71295463fb
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2024-09-24 22:04:50 +0500

    completed the slider component project

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Accordion from './components/Accordion/index'
 import RandomColorGenerator from './components/RandomColorGenerator/index'
 import StarRating from './components/StarRating';
+import ImageSlider from './components/ImageSlider';
 
 function App() {
   
@@ -11,7 +12,8 @@ function App() {
   <>
      {/* <Accordion/> */}
      {/* <RandomColorGenerator/> */}
-     <StarRating noOfStars={10}/>
+     {/* <StarRating noOfStars={10}/> */}
+     <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"}/>
   </>
    
   
